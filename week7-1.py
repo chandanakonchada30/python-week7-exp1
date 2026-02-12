c2db431ac252e7d98612c80479b1af5d5380c9f8
chandanakonchada30 <24331a05e2@mvgrce.edu.in>
2026-02-13 00:19:29 +0530

    Add files via upload

---
+++ week7-1.py
@@ -0,0 +1,14 @@
+#24331A05E2
+#to implement the following list functions a)len() b)extend() c)sort() d) append() e)insert() f)remove() .
+my_list = [10, 20, 30]
+print(" Length:" ,len(my_list))
+my_list.extend([40, 50])
+print(" Extend:",my_list)
+my_list.sort(reverse=True) 
+print(" Sort:",my_list)
+my_list.append(60)      
+print("Append:",my_list)
+my_list.insert(1, 15)    
+print(" Insert:",my_list)
+my_list.remove(30)       
+print("Remove:",my_list)
